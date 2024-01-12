e3e493e4a2427ddfb4b71307b749e7933101cdb1
joanserrats <joanserratsmollevi@gmail.com>
2024-01-12 15:46:43 +0100

    import dataset with click

--- scripts/repo_first_script.py
+++ scripts/repo_first_script.py
@@ -1,14 +1,31 @@
 """
-script to make updates in github
+Script to make updates in github
 """
+
 import pandas as pd
+import click
 
-
-def main():
+class FilteringClass:
     """
-    Main Function
+    Class for filtering
     """
-    pass
 
-if __name__ == "__main__":
+    def __init__(self,df):
+        self,df = df
+
+
+
+
+
+
+click.command(short_help='Parser to import datset')
+click.option('-f', '--file_name', required=True, help='File to import')
+def main(file_name):
+    """
+    Main function
+    """
+    df = pd.read_csv(file_name)
+    print(df.shape)
+
+if __name__ == "_main_":
     main()
