c6f3370f6dc806dba638bafa580fd7eabfc856c7
joanserrats <joanserratsmollevi@gmail.com>
2024-01-12 16:08:05 +0100

    new class for filtering

--- scripts/repo_first_script.py
+++ scripts/repo_first_script.py
@@ -10,22 +10,27 @@ class FilteringClass:
     Class for filtering
     """
 
-    def __init__(self,df):
-        self,df = df
+    def _init_(self,df):
+        self.df = df
 
+    def filter_price(self, price):
+        """
+        Filter price
+        """
+        return self.df[self.df['Price Starting With ($)'] < price]
 
-
-
-
-
-click.command(short_help='Parser to import datset')
-click.option('-f', '--file_name', required=True, help='File to import')
+@click.command(short_help='Parser to import datset')
+@click.option('-f', '--file_name', required=True, help='File to import')
 def main(file_name):
     """
     Main function
     """
     df = pd.read_csv(file_name)
-    print(df.shape)
+    
+    
+    result = FilteringClass(df).filter_price(12)
+    
+    print(result.shape)
 
-if __name__ == "_main_":
+if __name:__ == "_main_":
     main()
