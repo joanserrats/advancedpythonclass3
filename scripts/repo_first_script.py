3c35fb7c4aa6eee2f3d0269801cd1a7d5c8cc1dc
joanserrats <joanserratsmollevi@gmail.com>
2024-01-15 17:01:26 +0100

    write test for dataset and changes to script

--- scripts/repo_first_script.py
+++ scripts/repo_first_script.py
@@ -18,6 +18,17 @@ class FilteringClass:
         Filter price
         """
         return self.df[self.df['Price Starting With ($)'] < price]
+    
+
+def load_dataset(filename):
+    """
+    function to load dataset
+    """
+    extension = filename.rsplit('.', 1)[-1]
+    if extension == "csv":
+        return pd.read_csv(filename)
+    else:
+        raise TypeError(f"The extension is {extension} and not csv. Try again")
 
 @click.command(short_help='Parser to import datset')
 @click.option('-f', '--file_name', required=True, help='File to import')
@@ -25,7 +36,7 @@ def main(file_name):
     """
     Main function
     """
-    df = pd.read_csv(file_name)
+    df = load_dataset(input_file)
     
     
     result = FilteringClass(df).filter_price(12)
